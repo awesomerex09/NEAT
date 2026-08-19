fdbe111c1e65009ddf9a098eb199d642d5baa91a
VillainPrime <VillainPrime@example.com>
2026-08-19 18:27:06 +0800

    Updates

---
+++ js/firebase-config.js
@@ -0,0 +1,12 @@
+// NEAT — Firebase Configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDsWndjvt9VmgXNiNVi06fORfcqFy33rSo",
+  authDomain: "neat-ae516.firebaseapp.com",
+  projectId: "neat-ae516",
+  storageBucket: "neat-ae516.firebasestorage.app",
+  messagingSenderId: "879709911125",
+  appId: "1:879709911125:web:74b99056984b686631fcdc",
+  measurementId: "G-NV247R1ZJM"
+};
+
+window.firebaseConfig = firebaseConfig;
